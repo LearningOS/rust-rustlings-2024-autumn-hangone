8ce64d4eac41a2c5ab8d229c8dd9d885a76acc5d
hangone <56105779+hangone@users.noreply.github.com>
2024-10-20 23:08:32 +0800

    99/110: finish test8.rs

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -6,6 +6,7 @@ fn main() {
     // In tests7, we should set up an environment variable
     // called `TEST_FOO`. Print in the standard output to let
     // Cargo do it.
+
     let timestamp = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
@@ -16,6 +17,6 @@ fn main() {
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "Your command here, please checkout exercises/tests/build.rs";
+    let your_command = "rustc-cfg=feature=\"pass\"";
     println!("cargo:{}", your_command);
 }
